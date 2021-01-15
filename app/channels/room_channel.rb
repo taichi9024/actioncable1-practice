05bcb1ec0bc7d4d0c7300365ebdb90f5d4b1dbf8
taichi9024 <ebh48978@gmail.com>
2021-01-15 10:08:01 +0900

    [fin action cable]

---
+++ app/channels/room_channel.rb
@@ -0,0 +1,24 @@
+class RoomChannel < ApplicationCable::Channel
+  def subscribed
+    stream_from "room_channel"
+  end
+
+  def unsubscribed
+    # Any cleanup needed when channel is unsubscribed
+  end
+
+  def speak(data)
+    message = Message.create!(content: data["message"])
+    ActionCable.server.broadcast(
+      "room_channel", {message: render_mes(message)}
+    )
+  end
+
+  private
+  def render_mes(message)
+    ApplicationController.render(
+      partial: "messages/message",
+      locals: {message: message}
+    )
+  end
+end
